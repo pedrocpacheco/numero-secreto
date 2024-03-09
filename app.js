f41d97da7cf6cee66cbd3e3944af514c9687727f
pedrocpacheco <cpedropacheco@hotmail.com>
2024-03-09 11:20:28 -0300

    mudanca main 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brasil Portuguese Female', { rate: 1.2 });
+    responsiveVoice.speak(texto, 'Main Portuguese Female', { rate: 1.2 });
 }
 
 function exibirMensagemInicial() {
